251c4e0f89b91d5fdfd172169bda7f0e8616e769
GoldenCloudPc <jvladimir54@yahoo.com>
2023-12-07 20:26:55 +0100

    Update

--- index.js
+++ index.js
@@ -1,26 +1,58 @@
 const { spawn } = require('node:child_process');
 
-let child_process = spawn('cat ./cuvanje.txt',options); //Ne postoji cuvanje.txt
+// let child_process = spawn('cat ./cuvanje.txt',options); //Ne postoji cuvanje.txt
 
-child_process.stdin.write("Writted within child input",(error)=>{
-    if(error){
-        console.log(error);
-    }else{
-        console.log("Data writtend in the child");
-    }  
-})
+// child_process.stdin.write("Writted within child input",(error)=>{
+//     if(error){
+//         console.log(error);
+//     }else{
+//         console.log("Data writtend in the child");
+//     }  
+// })
 
-child_process.stdin.end();
+// child_process.stdin.end();
 
-child_process.stdout.on('data', (data,error)=>{ 
-    if(error){
-        console.log(error);
-    }else{
-        console.log(data.toString());
-    }
-})
+// child_process.stdout.on('data', (data,error)=>{ 
+//     if(error){
+//         console.log(error);
+//     }else{
+//         console.log(data.toString());
+//     }
+// })
 
 // Event handleri
-child_process.on('close',()=>{    //message, close...
-    console.log("Chilled process ended!");
-})
+// child_process.on('close',()=>{    //message, close...
+//     console.log("Chilled process ended!");
+// })
+
+//Import i export iz module odnosn oglobqalnog objekta
+
+// const { stdout,stdin,exit } = require('node:process');
+
+// let questions = [
+//     "What is yor name?",
+//     "What is your fav color",
+//     "What is your preferred programming language"
+// ]
+
+// let answers = [];
+
+// let ask = (i) => {
+//     stdout._write(`\n\n\n\ ${questions[i]}`);
+//     stdout._write(`  >  `);
+// } 
+
+// stdin.on('data',(answer)=>{
+//     answers.push(answer);
+//     if(answers.length < questions.length){
+//         ask(answers.length);
+//     }else{
+//         exit(0);
+//     }
+// });
+
+// process.on('exit',()=>{
+//     stdout.write(`${answers[0]} + ${answers[1]} + ${answers[2]}`);
+// })
+
+// ask(0);
