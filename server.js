47b57edd47626bf99a5102018a7e02cfe07c15c3
GoldenCloudPc <jvladimir54@yahoo.com>
2023-12-09 18:16:30 +0100

    Offloaded main process

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const app = express();
 const port = 4000;
+const { fork } = require('node:child_process');
 
-app.get('/:id', (req, res) => {
-  res.json({ "Persons id: ": req.params.id });
+app.get('/:id', (req, res) => { //NASTAVI
+
+    let child_process = fork('child.js');
+
+    child_process.send(req.params.id);
+
+    child_process.on('message',(procMess)=>{
+        res.json(procMess);
+    })
+
 });
-
 
 app.listen(port, () => {
   console.log(`Server is listening at ${port}`);
