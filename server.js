8b7aa4a0797ffd45b5038d6ede33ea0642461973
GoldenCloudPc <jvladimir54@yahoo.com>
2023-12-10 14:29:13 +0100

    Server handle child logic

--- server.js
+++ server.js
@@ -3,17 +3,27 @@ const app = express();
 const port = 4000;
 const { fork } = require('node:child_process');
 
-app.get('/:id', (req, res) => { //NASTAVI
+app.get('/linkedList/generate/:number', (req, res) => {
 
     let child_process = fork('child.js');
 
-    child_process.send(req.params.id);
+    child_process.send(req.params.number);
 
     child_process.on('message',(procMess)=>{
         res.json(procMess);
     })
-
 });
+
+// app.get('/linkedList/:number', (req, res) => {
+
+//     let child_process = fork('child.js');
+
+//     child_process.send(req.params.number);
+
+//     child_process.on('message',(procMess)=>{
+//         res.json(procMess);
+//     })
+// });
 
 app.listen(port, () => {
   console.log(`Server is listening at ${port}`);
