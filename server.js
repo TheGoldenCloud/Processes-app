ec52b8ea2811a3bbaf8cf6464ef0308aa9004857
GoldenCloudPc <jvladimir54@yahoo.com>
2023-12-16 14:41:59 +0100

    Added new rout that connects to linux remote server with help of child process

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const port = 4000;
-const { fork } = require('node:child_process');
+const { fork,spawn } = require('node:child_process');
 const { Worker } = require('node:worker_threads'); 
 
 app.get('/child/linkedList/:number', (req, res) => {
@@ -24,7 +24,32 @@ app.get('/thread/linkedList/:number', (req, res) => {
     thread.on('message',(data)=>{
       res.json(data);
     })    
+});
 
+app.get('/connect/:port/:username/:host', (req, res) => {
+
+  const conf = {
+    host: req.params.host,
+    port: req.params.port,
+    username: req.params.username,
+    privateKey: 'path-to-private-key.pem'
+  };
+
+  const sshCom = `ssh -p ${conf.port} ${conf.username}@${conf.host}`;
+  
+  const childProcess = spawn('cmd.exe', ['/c', 'start', 'cmd.exe', '/k', sshCom], { shell: true });
+  
+  childProcess.stdout.on('data', (data) => {
+    console.log(`Output: ${data}`);
+  });
+  
+  childProcess.stderr.on('data', (data) => {
+    console.error(`Error: ${data}`);
+  });
+  
+  childProcess.on('close', (code) => {
+    console.log(`Process error code ${code}, process exited`);
+  });
 });
 
 app.listen(port, () => {
